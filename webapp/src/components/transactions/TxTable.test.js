b0f4202c265dfbd8e358fd6cabbe6ccbc73c3ddc
kzlsakal <dorukgurleyen@gmail.com>
2021-01-22 21:54:06 -0600

    Add basic render test for TxTable

--- webapp/src/components/transactions/TxTable.test.js
+++ webapp/src/components/transactions/TxTable.test.js
@@ -1,8 +1,19 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import { TxTable } from './TxTable'
 
 describe('Transactions Table', () => {
   it('should show user "employee4" with amount "150"', () => {
-
+    const mockData = [{
+      id: '1',
+      userId: 'employee4',
+      description: 'test',
+      merchantId: 'Google',
+      debit: false,
+      credit: true,
+      amount: 15000
+    }]
+    render(<TxTable data={mockData} />)
+    expect(screen.getByTestId('transaction-1-amount')).toHaveTextContent('15000')
   })
-
 })
