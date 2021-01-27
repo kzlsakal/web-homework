bc7cd761f7f5360a1fcaf28676fd8ee11a419da0
kzlsakal <dorukgurleyen@gmail.com>
2021-01-27 02:09:02 -0600

    Add tests for companies

---
+++ elixir/test/homework/companies_test.exs
@@ -0,0 +1,75 @@
+defmodule Homework.CompaniesTest do
+  use Homework.DataCase
+
+  alias Homework.Companies
+
+  describe "companies" do
+    alias Homework.Companies.Company
+
+    @valid_attrs %{credit_line: 10_000_000, name: "some name"}
+    @update_attrs %{
+      credit_line: 20_000_000,
+      name: "some updated name"
+    }
+    @invalid_attrs %{credit_line: nil, name: nil}
+
+    def company_fixture(attrs \\ %{}) do
+      {:ok, company} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Companies.create_company()
+
+      company
+    end
+
+    test "list_companies/1 returns all companies" do
+      company = company_fixture()
+      assert Companies.list_companies([]) == [company]
+    end
+
+    test "get_company!/1 returns the company with given id" do
+      company = company_fixture()
+
+      IO.inspect(company)
+
+      assert Companies.get_company!(company.id) ==
+               Map.put(company, :available_credit, company.credit_line)
+    end
+
+    test "create_company/1 with valid data creates a company" do
+      assert {:ok, %Company{} = company} = Companies.create_company(@valid_attrs)
+      assert company.credit_line == 10_000_000
+      assert company.name == "some name"
+    end
+
+    test "create_company/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Companies.create_company(@invalid_attrs)
+    end
+
+    test "update_company/2 with valid data updates the company" do
+      company = company_fixture()
+      assert {:ok, %Company{} = company} = Companies.update_company(company, @update_attrs)
+      assert company.credit_line == 20_000_000
+      assert company.name == "some updated name"
+    end
+
+    test "update_company/2 with invalid data returns error changeset" do
+      company = company_fixture()
+      assert {:error, %Ecto.Changeset{}} = Companies.update_company(company, @invalid_attrs)
+
+      assert Map.put(company, :available_credit, company.credit_line) ==
+               Companies.get_company!(company.id)
+    end
+
+    test "delete_company/1 deletes the company" do
+      company = company_fixture()
+      assert {:ok, %Company{}} = Companies.delete_company(company)
+      assert_raise Ecto.NoResultsError, fn -> Companies.get_company!(company.id) end
+    end
+
+    test "change_company/1 returns a company changeset" do
+      company = company_fixture()
+      assert %Ecto.Changeset{} = Companies.change_company(company)
+    end
+  end
+end
