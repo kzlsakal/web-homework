41e9eb8f70e6b8f1c8bd6f73292e41af81084ef8
kzlsakal <dorukgurleyen@gmail.com>
2021-01-26 22:31:17 -0600

    Refactor tests with updated resolvers & args

--- elixir/test/homework/transactions_test.exs
+++ elixir/test/homework/transactions_test.exs
@@ -76,14 +76,14 @@ defmodule Homework.TransactionsTest do
       {:ok, transaction} =
         attrs
         |> Enum.into(valid_attrs)
-        |> Transactions.create_transaction()
+        |> Transactions.add_transaction()
 
       transaction
     end
 
     test "list_transactions/1 returns all transactions", %{valid_attrs: valid_attrs} do
       transaction = transaction_fixture(valid_attrs)
-      assert Transactions.list_transactions([]) == [transaction]
+      assert Transactions.list_transactions(%{_skip: 0, _limit: 10}) == [transaction]
     end
 
     test "get_transaction!/1 returns the transaction with given id", %{valid_attrs: valid_attrs} do
@@ -91,12 +91,12 @@ defmodule Homework.TransactionsTest do
       assert Transactions.get_transaction!(transaction.id) == transaction
     end
 
-    test "create_transaction/1 with valid data creates a transaction", %{
+    test "add_transaction/1 with valid data creates a transaction", %{
       valid_attrs: valid_attrs,
       merchant1: merchant1,
       user1: user1
     } do
-      assert {:ok, %Transaction{} = transaction} = Transactions.create_transaction(valid_attrs)
+      assert {:ok, %Transaction{} = transaction} = Transactions.add_transaction(valid_attrs)
       assert transaction.amount == 42
       assert transaction.credit == true
       assert transaction.debit == true
@@ -108,7 +108,7 @@ defmodule Homework.TransactionsTest do
     test "create_transaction/1 with invalid data returns error changeset", %{
       invalid_attrs: invalid_attrs
     } do
-      assert {:error, %Ecto.Changeset{}} = Transactions.create_transaction(invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Transactions.add_transaction(invalid_attrs)
     end
 
     test "update_transaction/2 with valid data updates the transaction", %{
